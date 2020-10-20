f041a59eb7630bf2d11135a3cc131ba146689158
Dmitry Orlov <feanarokurufinve@gmail.com>
2020-10-21 01:56:44 +0300

    Added save word functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,70 @@
 import React from 'react';
 import './App.css';
+import Database from './db';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.database = new Database();
     this.state = {
       addingWord: false,
+      word: "",
+      gender: "m",
+      spellings: "",
+      part_of_speech: "noun",
+      meanings: [
+        {
+          meaning: "",
+          examples: ""
+        }
+      ]
     };
+  }
+
+  addWord = event => {
+    event.preventDefault();
+    this.database.saveWord({
+      word: this.state.word,
+      gender: this.state.gender,
+      spellings: this.state.spellings.split(','),
+      part_of_speech: this.state.part_of_speech,
+      meanings: this.state.meanings,
+      status: "draft"
+    })
+  }
+
+  updateInput = e => {
+    this.setState({
+      [e.target.id]: e.target.value
+    });
+  }
+
+  addMeaning = e => {
+    this.setState({
+      meanings: [
+        ...this.state.meanings,
+        {
+          meaning: "",
+          examples: ""
+        }
+      ]
+    })
+  }
+
+  updateMeaning = (idx, value) => {
+    let meanings = this.state.meanings;
+    meanings[idx].meaning = value;
+    this.setState({
+      meanings: meanings
+    })
+  }
+
+  updateMeaningExamples = (idx, value) => {
+    let meanings = this.state.meanings;
+    meanings[idx].examples = value;
+    this.setState({
+      meanings: meanings
+    })
   }
 
   render() {
@@ -34,47 +92,65 @@ class App extends React.Component {
     let wordAddForm = (
       <div className="row">
         <div className="col-12">
-          <form>
+          <form onSubmit={this.addWord}>
             <div className="form-group">
               <label htmlFor="word">Слово</label>
-              <input type="text" className="form-control" id="word" placeholder="हिंदी" />
+              <input type="text" className="form-control" id="word" placeholder="हिंदी"
+                required={ true } onChange={this.updateInput} />
             </div>
             <div className="form-group">
               <label htmlFor="spellings">Альтернативные написания</label>
-              <input type="text" className="form-control" id="spellings" placeholder="हिंदी, हिन्दी" />
+              <input type="text" className="form-control" id="spellings" placeholder="हिंदी, हिन्दी" onChange={this.updateInput} />
             </div>
             <div className="form-group">
               <label htmlFor="gender">Род</label>
-              <select className="form-control" id="gender">
+              <select className="form-control" id="gender" onChange={this.updateInput}>
                 <option value="f">мужской</option>
                 <option value="m">женский</option>
               </select>
             </div>
             <div className="form-group">
               <label htmlFor="part_of_speech">Часть речи</label>
-              <select className="form-control" id="part_of_speech">
+              <select className="form-control" id="part_of_speech" onChange={this.updateInput}>
                 <option value="noun">существительное</option>
                 <option value="adjective">прилагательное</option>
               </select>
             </div>
             <div className="form-group">
-              <label htmlFor="meanings">Значения</label>
-              <div className="input-group">
-                <textarea className="form-control" id="meanings"
-                  placeholder="хинди (язык)" rows="3"></textarea>
-                <textarea className="form-control" id="meanings_example"
-                  placeholder="हम हिंदी बोलते हैं। Мы говорим на хинди." rows="3"></textarea>
-                <div className="input-group-append">
-                  <button className="btn btn-outline-secondary" type="button">
-                    <i className="fas fa-plus"></i>
-                  </button>
-                </div>
-              </div>
-              <div className="col-12 my-3 text-center">
-                <button className="btn btn-outline-primary" type="button">
-                  Отправить
-                </button>
-              </div>
+              <label>Значения</label>
+              {
+                this.state.meanings.map(
+                  (value, index) => {
+                    return (
+                      <div className="input-group" key={ "meaning-input-group-" + index }>
+                        <textarea className="form-control" id={ "meanings-" + index }
+                          placeholder="хинди (язык)" rows="3" required={ true }
+                          value={ value.meaning } onChange={event => this.updateMeaning(index, event.target.value)}>
+                        </textarea>
+                        <textarea className="form-control" id={ "meanings_example" + index }
+                          placeholder="हम हिंदी बोलते हैं। Мы говорим на хинди." rows="3" required={ true }
+                          value={ value.examples } onChange={event => this.updateMeaningExamples(index, event.target.value)}>
+                        </textarea>
+                        <div className="input-group-append">
+                          {
+                            (index === this.state.meanings.length - 1) ? (
+                              <button className="btn btn-outline-secondary" type="button"
+                                onClick={this.addMeaning}>
+                                <i className="fas fa-plus"></i>
+                              </button>
+                            ) : ""
+                          }
+                        </div>
+                      </div>
+                    )
+                  }
+                )
+              }
+            </div>
+            <div className="col-12 my-3 text-center">
+              <button className="btn btn-outline-primary" type="submit">
+                Отправить
+              </button>
             </div>
           </form>
         </div>
---
+++ src/db.js
@@ -0,0 +1,30 @@
+import firebase from 'firebase';
+
+class Database {
+    constructor() {
+        if (process.env.REACT_APP_FIREBASE_API_KEY) {
+            const config = {
+                apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+                authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+                databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+                projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+                storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+                messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+                appId: process.env.REACT_APP_FIREBASE_APP_ID,
+                measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+            };
+            if (!firebase.apps.length) {
+                firebase.initializeApp(config);
+            }
+        } else {
+            console.log("No Firebase config found!");
+        }
+    }
+
+    saveWord(word) {
+        const db = firebase.firestore();
+        db.collection("articles").add(word);  
+    }
+}
+
+export default Database;
